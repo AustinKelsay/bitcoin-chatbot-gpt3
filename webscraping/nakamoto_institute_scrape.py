a8cd1ee2a32789fb4e67a4a46434b6447c2e7156
austinkelsay <austinkelsay@yahoo.com>
2021-08-02 13:30:20 -0600

    Iterating through anchor tags

--- webscraping/nakamoto_institute_scrape.py
+++ webscraping/nakamoto_institute_scrape.py
@@ -7,7 +7,11 @@ from selenium.webdriver.support.expected_conditions import presence_of_element_l
 #This example requires Selenium WebDriver 3.13 or newer
 with webdriver.Firefox() as driver:
     wait = WebDriverWait(driver, 10)
-    driver.get("https://google.com/ncr")
-    driver.find_element(By.NAME, "q").send_keys("cheese" + Keys.RETURN)
-    first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3")))
-    print(first_result.get_attribute("textContent"))
+    driver.get("https://nakamotoinstitute.org/literature/")
+
+    articles = driver.find_elements_by_xpath("//a[@href]")
+    for article in articles:
+        print(article.get_attribute("href"))
+        #driver.find_element(By.NAME, "a").click()
+    # first_result = wait.until(presence_of_element_located((By.CLASS_NAME, "col-sm-9")))
+    # print(first_result)
