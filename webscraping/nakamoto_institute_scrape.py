b5953ba15af74523743ac57150026b38408ced1d
austinkelsay <austinkelsay@yahoo.com>
2021-08-02 13:50:10 -0600

    Makes sense but I'm stuck

--- webscraping/nakamoto_institute_scrape.py
+++ webscraping/nakamoto_institute_scrape.py
@@ -5,13 +5,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
 #This example requires Selenium WebDriver 3.13 or newer
+parent_articles = []
+child_articles = []
+
 with webdriver.Firefox() as driver:
     wait = WebDriverWait(driver, 10)
     driver.get("https://nakamotoinstitute.org/literature/")
 
     articles = driver.find_elements_by_xpath("//a[@href]")
     for article in articles:
-        print(article.get_attribute("href"))
-        #driver.find_element(By.NAME, "a").click()
-    # first_result = wait.until(presence_of_element_located((By.CLASS_NAME, "col-sm-9")))
-    # print(first_result)
+        if "/literature/" in article.get_attribute("href"):
+            article.click()
+            wait.until(presence_of_element_located((By.XPATH, "//a[@href]")))
+            children = driver.find_elements_by_xpath("//a[@href]")
+            for child in children:
+                if child.text == "HTML":
+                    child_articles.append(child.get_attribute("href"))
+                    break
+
+print(child_articles)
