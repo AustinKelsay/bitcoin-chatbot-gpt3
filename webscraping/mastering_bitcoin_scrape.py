cbd0bb2917114775ccd28bd00702cbd5c973fc49
austinkelsay <austinkelsay@yahoo.com>
2021-08-03 13:07:45 -0600

    Finished mastering bitcoin scraping and created json outfile for training data

--- webscraping/mastering_bitcoin_scrape.py
+++ webscraping/mastering_bitcoin_scrape.py
@@ -13,3 +13,29 @@ data =[]
 with webdriver.Firefox() as driver:
     wait = WebDriverWait(driver, 10)
     driver.get("https://github.com/bitcoinbook/bitcoinbook/blob/develop/book.asciidoc")
+
+    # Get parent articles
+    articles = driver.find_elements_by_xpath("//a[@href]")
+    for article in articles:
+        if ".asciidoc" in article.text and "appdx" not in article.text:
+            parent_articles.append(article.get_attribute("href"))
+
+    for article in parent_articles:
+            driver.get(article)
+            section = driver.find_elements_by_xpath("//p")
+            header = driver.find_elements_by_xpath("//h2")
+
+            chapter = []
+
+            for sec in section:
+                j = {
+                    "prompt": header[1].text,
+                    "completion": sec.text
+                }
+                data.append(j)
+
+
+with open('mastering_bitcoin.json', 'w') as outfile:    
+    for obj in data:
+        json.dump(obj, outfile)
+        outfile.write('\n')
