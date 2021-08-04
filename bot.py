a4a1dfa00617dcf3aa0a4403f8847c8045bc511f
austinkelsay <austinkelsay@yahoo.com>
2021-08-04 14:16:14 -0600

    First iteration of bot file

---
+++ bot.py
@@ -0,0 +1,38 @@
+import json
+from dotenv import load_dotenv
+import openai
+import os
+
+load_dotenv()
+openai.api_key = os.getenv("GPT3_API_KEY")
+completion = openai.Completion()
+
+model = os.getenv("FINE_TUNE_MODEL_V1")
+
+
+start_chat_log = '''World: How does proof-of-work work in Bitcoin?
+AI: A proof of work is a piece of data which is difficult (costly, time-consuming) to produce but easy for others to verify and which satisfies certain requirements. Producing a proof of work can be a random process with low probability so that a lot of trial and error is required on average before a valid proof of work is generated. Bitcoin uses the Hashcash proof of work system.
+World: What is a hash?
+AI: A hash is a function which takes a piece of data and converts it into a number.
+World: What is SHA-256?
+AI: SHA-256 is a specific hash function which takes a piece of data and converts it into a number.
+World: How is Bitcoin verifiable?
+AI: A block is a piece of data which contains a hash of the previous block and a set of transactions. The block is verified by a proof of work which is a piece of data which is difficult to produce but easy for others to verify and which satisfies certain requirements. Producing a proof of work can be a random process with low probability so that a lot of trial and error is required on average before a valid proof of work is generated. Bitcoin uses the Hashcash proof of work system.
+'''
+
+def append_interaction_to_chat_log(question, answer, chat_log=None):
+    if chat_log is None:
+        chat_log = start_chat_log
+    return f'{chat_log}World: {question}\nAI: {answer}\n'
+
+
+def ask(question, chat_log=None):
+    if chat_log is None:
+        chat_log = start_chat_log
+    prompt = f'{chat_log}World: {question}\nAI:'
+    response = completion.create(
+        prompt=prompt, model=model, stop=['\nWorld'], temperature=0.7,
+        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
+        max_tokens=700)
+    answer = response.choices[0].text.strip()
+    return answer
