fb7c885882a982c4420636ec3ddb7f1131aba11e
austinkelsay <austinkelsay@yahoo.com>
2021-08-07 15:57:55 -0600

    Trying to get some results with summarization

--- bot.py
+++ bot.py
@@ -35,4 +35,27 @@ def ask(question, chat_log=None):
         top_p=1, frequency_penalty=0, presence_penalty=0.3, best_of=1,
         max_tokens=500)
     answer = response.choices[0].text.strip()
+
+    question_and_answer = question + "\n\"\"\"\n" + answer
+    sum = summarize(question_and_answer)
+    answer_obj = {
+        "Not sum": answer,
+        "Sum": sum
+    }
+    return(answer_obj)
+
+def summarize(q):
+    user_prompt = q
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=f"My second grader asked me what this passage means:\n\"\"\"\nJupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus.\n\"\"\"\nI rephrased it for him, in plain language a second grader can understand:\n\"\"\"\n{user_prompt}",
+        temperature=0.2,
+        max_tokens=100,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\"\"\""]
+    )
+    answer = response.choices[0].text.strip()
     return answer
+    
