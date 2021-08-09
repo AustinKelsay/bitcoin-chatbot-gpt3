9b6efe7fb3e2726838a66b11afdee23e9885611f
austinkelsay <austinkelsay@yahoo.com>
2021-08-09 11:05:26 -0600

    Got server running locally

--- keep_alive.py
+++ keep_alive.py
@@ -1,12 +1,25 @@
 from flask import Flask
 from flask import request
 from threading import Thread
-from bot import ask
+import bot
 
 app = Flask('')
 
+@app.route('/')
+def main():
+    return "Your bot is alive!"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
+
 @app.route('/ask', methods=['POST'])
 def ask_bot():
-    question = request.form['question']
-    answer = ask(question)
+    question = request.json["question"]
+    answer = bot.ask(question)
+    print(answer)
     return answer
+    
