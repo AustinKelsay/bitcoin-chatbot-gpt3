98ae3cd5289e30290430176e1936975df5e3d8bd
austinkelsay <austinkelsay@yahoo.com>
2021-10-05 16:42:20 -0600

    Finally got this scrape to work fully and format data correctly! Now I jsut need to clean up the code a bit

--- webscraping/articles/nakamoto_institute_articles.py
+++ webscraping/articles/nakamoto_institute_articles.py
@@ -12,7 +12,9 @@ knowledge_sources = ('https://nakamotoinstitute.org/literature/', 'https://nakam
 articles = []
 blacklisted_urls = ['https://nakamotoinstitute.org/mempool/authors', 'https://nakamotoinstitute.org/mempool/series', 'https://nakamotoinstitute.org/mempool/feed/', 
 'https://nakamotoinstitute.org/', 'https://satoshi.nakamotoinstitute.org/', 'https://nakamotoinstitute.org/mempool/']
-data =[]
+data = []
+
+driver = webdriver.Firefox()
 
 def tag_visible(element):
     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
@@ -35,8 +37,8 @@ with webdriver.Firefox() as driver:
             # Scrape is slightly different on this page
             anchors = driver.find_elements_by_xpath("//a[@href]")
             for link in anchors:
-                # Avoid all pages that arent an article
-                if link.get_attribute('href') in blacklisted_urls:
+                # Avoid all pages that arent an article text page
+                if link.get_attribute('href') in blacklisted_urls or 'https://nakamotoinstitute.org/mempool/series/' in link.get_attribute('href'):
                     pass
                 elif 'https://nakamotoinstitute.org/mempool/' in link.get_attribute('href'):
                     articles.append(link.get_attribute('href'))
@@ -44,27 +46,27 @@ with webdriver.Firefox() as driver:
             # Get all articles that arent pdf's
             anchors = driver.find_elements_by_xpath("//a[@href]")
             for link in anchors:
-                if link.text in ["txt", "HTML"]:
+                if link.text == "HTML":
                     articles.append(link.get_attribute('href'))
 
+count = 0
 for article in articles:
-    try:
-        driver = webdriver.Firefox()
+    with webdriver.Firefox() as driver:
+        wait = WebDriverWait(driver, 5)
         driver.get(article)
-        html = driver.page_source
-        article_text = text_from_html(html)
-        # Iterate through list of strings
-        for text in article_text:
+        title = driver.find_element_by_xpath("//h1").text
+        body = driver.find_elements_by_xpath("//p")
+        completion = ''
+        for paragraph in body:
             obj = {
                 'link': article,
-                'text': text
+                'title': title,
+                'text': paragraph.text
             }
             data.append(obj)
         driver.close()
-    except:
-        print("ERROR")
-        print('\n')
-        print(article)
+        count += 1
+        print(count)
 
 # Output training data to json file
 with open('nakamoto_institute_scrape.json', 'w') as outfile:
