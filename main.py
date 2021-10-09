040be22590ca470d9f79702a994230bfcc3fcc28
austinkelsay <austinkelsay@yahoo.com>
2021-10-09 10:51:22 -0600

    Testing with bot route commented out

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
 from threading import Thread
-from app.bot import ask
+# from app.bot import ask
 
 app = Flask(__name__)
 
@@ -12,12 +12,12 @@ def main():
 def run():
     app.run(host="0.0.0.0", port=8080)
 
-@app.route('/ask', methods=['POST'])
-def ask_bot():
-    question = request.json["question"]
-    answer = ask(question)
-    # run keep_alive everytime there is a request
-    return answer
+# @app.route('/ask', methods=['POST'])
+# def ask_bot():
+#     question = request.json["question"]
+#     answer = ask(question)
+#     # run keep_alive everytime there is a request
+#     return answer
 
 if __name__ == "__main__":
     app.run()
