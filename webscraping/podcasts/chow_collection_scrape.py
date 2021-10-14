d489294a54c6a8522d45cc85b83f33d6f54ac02e
austinkelsay <austinkelsay@yahoo.com>
2021-10-13 18:49:03 -0600

    The scrape is working fully as expected

--- webscraping/podcasts/chow_collection_scrape.py
+++ webscraping/podcasts/chow_collection_scrape.py
@@ -6,6 +6,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_all_elements_located, presence_of_element_located, element_to_be_clickable
 
+# articles that arent podcast transcripts
+article_blacklist = ["A Private Collector’s Guide to Art Collecting", "An introduction to /r/privatestudyrooms", 
+'Step-by-step: Documenting Anthony Gunin’s creation of The Most Holy Theotokos “Glykophilousa” icon', "Legacy",
+"Thoughts on Art Collecting", "Christ Pantocrator mosaic by Yury Yarin"]
 article_links = []
 data = []
 
@@ -27,6 +31,7 @@ with webdriver.Firefox() as driver:
             driver.find_element_by_xpath("// button[contains(text(),'Show more')]").click()
             time.sleep(10)
     except:
+        wait.until(presence_of_all_elements_located((By.XPATH, "//a[@class='eh bw']")))
         articles = driver.find_elements_by_xpath("//a[@class='eh bw']")
         for article in articles:
             article_links.append(article.get_attribute("href"))
@@ -37,16 +42,18 @@ with webdriver.Firefox() as driver:
                 wait.until(presence_of_all_elements_located((By.XPATH, "//p")))
                 text = driver.find_elements_by_xpath("//p")
                 title = driver.find_element_by_xpath("//h1")
-                for section in text:
-                    try:
-                        j = {
-                            "link": link,
-                            "title": title.text,
-                            "text": section.text
-                        }
-                        data.append(j)
-                    except:
-                        print("Error")
+                podcast_link = driver.find_elements_by_xpath("//a[@class='eh jf']")
+                if title.text not in article_blacklist:
+                    for section in text:
+                        try:
+                            j = {
+                                "link": podcast_link,
+                                "title": title.text,
+                                "text": section.text
+                            }
+                            data.append(j)
+                        except:
+                            print("Error")
 
 with open('./datasets/chow_collection_scrape.json', 'w') as outfile:    
     for obj in data:
