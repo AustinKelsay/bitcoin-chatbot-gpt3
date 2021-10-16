4fcdb5758a8f499162d6c52a6329492286590ebb
austinkelsay <austinkelsay@yahoo.com>
2021-10-16 16:46:37 -0600

    starting on script for uploading file

--- gpt3/upload_training_data.py
+++ gpt3/upload_training_data.py
@@ -1,11 +1,20 @@
 import os
+import json
 import openai
 from dotenv import load_dotenv
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.File.create(
-  file=open("puppy.jsonl"),
-  purpose='answers'
+  file=open("./datasets/openai_datasets/chow_collection_scrape.jsonl"),
+  purpose='fine-tune'
 )
 
+# # Examine files and add most recent file id to .env
+print(openai.File.list())
+openai_file_list = openai.File.list()
+new_file = openai_file_list['data'][-1]['id']
+
+# with open("gpt3/.env", 'a') as outfile:    
+#         outfile.write('\n')
+#         json.dump('FINE_TUNE_MODEL='+new_file, outfile, separators=('"', ''))
