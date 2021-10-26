eaa098613ad8d997d7cc67f123528ca107b62c11
austinkelsay <austinkelsay@yahoo.com>
2021-10-26 11:57:06 -0600

    Switching back to V5 since stops are different now

--- app/bot.py
+++ app/bot.py
@@ -8,7 +8,7 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 completion = openai.Completion()
 
-model = os.getenv("FINE_TUNE_MODEL_V1")
+model = os.getenv("FINE_TUNE_MODEL_V5")
 
 
 start_chat_log = '''User: How does proof-of-work work in Bitcoin?\nBot: A proof of work is a piece of data which is difficult (costly, time-consuming) to produce but easy for others to verify and which satisfies certain requirements. Producing a proof of work can be a random process with low probability so that a lot of trial and error is required on average before a valid proof of work is generated. Bitcoin uses the Hashcash proof of work system.\nUser: What is a hash?\nBot: A hash is a function which takes a piece of data and converts it into a number.\nUser: What is SHA-256?\nBot: SHA-256 is a specific hash function which takes a piece of data and converts it into a number.\nUser: What is a block?\nBot: A block is a piece of data which contains a hash of the previous block and a set of transactions. These blocks make up the blockchain which is Bitcoin's distributed ledger with the full history and cryptographic proof of all of the previous transactions.\nUser: do you have any trading advice for me?\nBot: Trading is risky and I dont give financial advice, I only suggest stacking Bitcoin and holding your own keys (Not your keys not your Bitcoin)\nUser: Thank you for the help!\nBot: You're welcome, come back anytime!'''
