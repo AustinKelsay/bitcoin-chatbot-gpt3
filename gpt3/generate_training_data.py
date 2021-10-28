6ec82b4eb2712b32628455cf311e97e2f916e6fa
austinkelsay <austinkelsay@yahoo.com>
2021-10-28 16:54:53 -0600

    Added text cleanup to training data generation script

--- gpt3/generate_training_data.py
+++ gpt3/generate_training_data.py
@@ -1,4 +1,5 @@
 import json
+import string
 
 def generate(prompts):
     openai_data = []
@@ -8,11 +9,13 @@ def generate(prompts):
             if count % 2 == 0:
                 # Make sure we're not grabbing empty text (This will skip over prompt/completions that are empty, have a podcast link, or are an outro to the podcast)
                 if prompts[count].text != "" and prompts[count+1].text != "":
-                    prompt = prompts[count].text
-                    completion = prompts[count+1].text
+                    prompt_text = prompts[count].text
+                    cleaned_prompt_text = prompt_text.translate(str.maketrans('', '', string.punctuation))
+                    completion_text = prompts[count+1].text
+                    cleaned_completion_text = completion_text.translate(str.maketrans('', '', string.punctuation))
                     j = {
-                        "prompt": f"{prompt}\n",
-                        "completion": f"{completion}\n"
+                        "prompt": f"{cleaned_prompt_text}\n",
+                        "completion": f"{cleaned_completion_text}\n"
                     }
                     openai_data.append(j)
         except:
