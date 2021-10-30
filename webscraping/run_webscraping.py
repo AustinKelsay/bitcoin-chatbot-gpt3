d694a47ba4ebd9d674160c61c9aafe74d28cac09
austinkelsay <austinkelsay@yahoo.com>
2021-10-30 11:50:33 -0600

    Created parent webscraping script

---
+++ webscraping/run_webscraping.py
@@ -0,0 +1,6 @@
+from articles.nakamoto_institute_articles import NakamotoInstituteArticles
+from articles.mastering_bitcoin_scrape import MasteringBitcoinScraper
+
+if __name__ == '__main__':
+    NakamotoInstituteArticles()
+    MasteringBitcoinScraper()
