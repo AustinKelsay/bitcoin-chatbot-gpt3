60e6402691d65759bb70756f9d37279bb7933124
austinkelsay <austinkelsay@yahoo.com>
2021-10-30 11:59:51 -0600

    Reworked scripts to be able to be ran for parent

--- webscraping/articles/mastering_bitcoin_scrape.py
+++ webscraping/articles/mastering_bitcoin_scrape.py
@@ -11,33 +11,34 @@ parent_articles = []
 child_articles = []
 data =[]
 
-with webdriver.Firefox() as driver:
-    wait = WebDriverWait(driver, 10)
-    driver.get("https://github.com/bitcoinbook/bitcoinbook/blob/develop/book.asciidoc")
+def Mastering_bitcoin_scrape():
+    with webdriver.Firefox() as driver:
+        wait = WebDriverWait(driver, 10)
+        driver.get("https://github.com/bitcoinbook/bitcoinbook/blob/develop/book.asciidoc")
 
-    # Get parent articles
-    articles = driver.find_elements_by_xpath("//a[@href]")
-    for article in articles:
-        if ".asciidoc" in article.text and "appdx" not in article.text:
-            parent_articles.append(article.get_attribute("href"))
+        # Get parent articles
+        articles = driver.find_elements_by_xpath("//a[@href]")
+        for article in articles:
+            if ".asciidoc" in article.text and "appdx" not in article.text:
+                parent_articles.append(article.get_attribute("href"))
 
-    for article in parent_articles:
-            driver.get(article)
-            text = driver.find_elements_by_xpath("//p")
-            # Get the name of the chapter which is index 1 in the list of h2 elements
-            chapter = driver.find_elements_by_xpath("//h2")
-            # Trigger bitcoin chatbot training data generation
-            generate(text)
-            for section in text:
-                j = {
-                    "link": article,
-                    "title": "Mastering bitcoin - " + chapter[1].text,
-                    "text": section.text
-                }
-                data.append(j)
+        for article in parent_articles:
+                driver.get(article)
+                text = driver.find_elements_by_xpath("//p")
+                # Get the name of the chapter which is index 1 in the list of h2 elements
+                chapter = driver.find_elements_by_xpath("//h2")
+                # Trigger bitcoin chatbot training data generation
+                generate(text)
+                for section in text:
+                    j = {
+                        "link": article,
+                        "title": "Mastering bitcoin - " + chapter[1].text,
+                        "text": section.text
+                    }
+                    data.append(j)
 
 
-with open('./datasets/mastering_bitcoin_scrape.json', 'w') as outfile:    
-    for obj in data:
-        json.dump(obj, outfile)
-        outfile.write('\n')
+    with open('./datasets/mastering_bitcoin_scrape.json', 'w') as outfile:    
+        for obj in data:
+            json.dump(obj, outfile)
+            outfile.write('\n')
--- webscraping/run_webscraping.py
+++ webscraping/run_webscraping.py
@@ -1,6 +1,6 @@
-from articles.nakamoto_institute_articles import NakamotoInstituteArticles
-from articles.mastering_bitcoin_scrape import MasteringBitcoinScraper
+from articles.nakamoto_institute_articles import Nakamoto_institute_articles
+from articles.mastering_bitcoin_scrape import Mastering_bitcoin_scrape
 
 if __name__ == '__main__':
-    NakamotoInstituteArticles()
-    MasteringBitcoinScraper()
+    Nakamoto_institute_articles()
+    Mastering_bitcoin_scrape()
