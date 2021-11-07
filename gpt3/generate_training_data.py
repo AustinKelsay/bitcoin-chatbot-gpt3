3a44cf4e1cb70d6e4ad0edc5683b37ceba8a67fa
austinkelsay <austinkelsay@yahoo.com>
2021-11-07 15:50:27 -0700

    Trying a simpler way I think

--- gpt3/generate_training_data.py
+++ gpt3/generate_training_data.py
@@ -15,7 +15,7 @@ def generate(prompts):
                     cleaned_completion_text = completion_text.translate(str.maketrans('', '', string.punctuation))
                     # Get rid of any text that is shorter than 35 chars
                     # Get rid of any text that has no whitespace in it
-                    if len(cleaned_prompt_text) > 75 and cleaned_prompt_text.find(" ") != -1 and len(cleaned_completion_text) > 75 and cleaned_completion_text.find(" ") != -1:
+                    if len(cleaned_prompt_text) > 50 and cleaned_prompt_text.find(" ") != -1 and len(cleaned_completion_text) > 50 and cleaned_completion_text.find(" ") != -1:
                         # Remove all non ascii chars
                         # prompts
                         strencode_prompt = cleaned_prompt_text.encode("ascii", "ignore")
@@ -23,11 +23,12 @@ def generate(prompts):
                         # completions
                         strencode_completion = cleaned_completion_text.encode("ascii", "ignore")
                         strdecode_completion = strencode_completion.decode()
-                        j = {
-                            "prompt": f"{strdecode_prompt} ->",
-                            "completion": f"{strdecode_completion}\n"
-                        }
-                        openai_data.append(j)
+                        if len(strdecode_completion) > strdecode_completion.count(' ') and len(strdecode_prompt) > strdecode_prompt.count(' '):
+                            j = {
+                                "prompt": f"{strdecode_prompt} ->",
+                                "completion": f"{strdecode_completion}\n"
+                            }
+                            openai_data.append(j)
         except:
             print("Error")
 
