ce31ed67fc99e4ee19a711f0f3dce67c0317c1bb
austinkelsay <austinkelsay@yahoo.com>
2021-11-13 19:55:12 -0700

    Got rid of whitespaces but now I'm getting null values...

---
+++ gpt3/clean_text.py
@@ -0,0 +1,5 @@
+
+def clean(text):
+    num_of_letters = len(text) - text.count(" ")
+    if text.count(" ") < num_of_letters:
+        return text
--- gpt3/generate_training_data.py
+++ gpt3/generate_training_data.py
@@ -1,6 +1,7 @@
 import json
 import string
 import re
+from .clean_text import clean
 
 def generate(prompts):
     openai_data = []
@@ -14,7 +15,7 @@ def generate(prompts):
                     cleaned_prompt_text = prompt_text.translate(str.maketrans('', '', string.punctuation))
                     completion_text = prompts[count+1].text
                     cleaned_completion_text = completion_text.translate(str.maketrans('', '', string.punctuation))
-                    # Get rid of any text that is shorter than 35 chars
+                    # Get rid of any text that is shorter than 50 chars
                     # Get rid of any text that has no whitespace in it
                     if len(cleaned_prompt_text) > 50 and cleaned_prompt_text.find(" ") != -1 and len(cleaned_completion_text) > 50 and cleaned_completion_text.find(" ") != -1:
                         # Remove all non ascii chars
@@ -25,17 +26,16 @@ def generate(prompts):
                         strencode_completion = cleaned_completion_text.encode("ascii", "ignore")
                         strdecode_completion = strencode_completion.decode()
                         # Filter out any string with more than one white space in between characters
-                        re.sub("\s\s+", ' ', strdecode_prompt)
-                        re.sub("\s\s+", ' ', strdecode_completion)
-                        # Get rid of any text that has more whitespaces than chars
-                        prompt_num_of_letters = len(strencode_prompt) - strencode_prompt.count(" ")
-                        completion_num_of_letters = len(strencode_completion) - strencode_completion.count(" ")
-                        if len(strencode_prompt) / 4 > prompt_num_of_letters and len(strencode_completion) / 4 > completion_num_of_letters:
-                            j = {
-                                "prompt": f"{strdecode_prompt}\n\n###\n\n",
-                                "completion": strdecode_completion
-                            }
-                            openai_data.append(j)
+                        re.sub(" +", ' ', strdecode_prompt)
+                        re.sub(" +", ' ', strdecode_completion)
+                        final_prompt = clean(strdecode_prompt)
+                        final_completion = clean(strdecode_completion)
+                        j = {
+                            "prompt": f"{final_prompt}\n\n###\n\n",
+                            "completion": final_completion
+                        }
+                        openai_data.append(j)
+
         except:
             print("Error")
 
