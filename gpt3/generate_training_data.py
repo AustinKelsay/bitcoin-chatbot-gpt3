c87cf1d0513e22f59e660d74f70179aaa53ba68f
austinkelsay <austinkelsay@yahoo.com>
2021-11-15 20:02:44 -0700

    Editing training data generation function to keep puncation in

--- gpt3/generate_training_data.py
+++ gpt3/generate_training_data.py
@@ -12,18 +12,18 @@ def generate(prompts):
                 # Make sure we're not grabbing empty text (This will skip over prompt/completions that are empty, have a podcast link, or are an outro to the podcast)
                 if prompts[count].text != "" and prompts[count+1].text != "":
                     prompt_text = prompts[count].text
-                    cleaned_prompt_text = prompt_text.translate(str.maketrans('', '', string.punctuation))
+                    # cleaned_prompt_text = prompt_text.translate(str.maketrans('', '', string.punctuation))
                     completion_text = prompts[count+1].text
-                    cleaned_completion_text = completion_text.translate(str.maketrans('', '', string.punctuation))
+                    # cleaned_completion_text = completion_text.translate(str.maketrans('', '', string.punctuation))
                     # Get rid of any text that is shorter than 50 chars
                     # Get rid of any text that has no whitespace in it
-                    if len(cleaned_prompt_text) > 50 and cleaned_prompt_text.find(" ") != -1 and len(cleaned_completion_text) > 50 and cleaned_completion_text.find(" ") != -1:
+                    if len(prompt_text) > 50 and prompt_text.find(" ") != -1 and len(completion_text) > 50 and completion_text.find(" ") != -1:
                         # Remove all non ascii chars
                         # prompts
-                        strencode_prompt = cleaned_prompt_text.encode("ascii", "ignore")
+                        strencode_prompt = prompt_text.encode("ascii", "ignore")
                         strdecode_prompt = strencode_prompt.decode()
                         # completions
-                        strencode_completion = cleaned_completion_text.encode("ascii", "ignore")
+                        strencode_completion = completion_text.encode("ascii", "ignore")
                         strdecode_completion = strencode_completion.decode()
                         # Filter out any string with more than one white space in between characters
                         re.sub(" +", ' ', strdecode_prompt)
